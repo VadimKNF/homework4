39a4638943db13910739c606f309bf8ad62dd5df
Batya <safgg7@mail.ru>
2019-10-05 22:02:14 +0400

    refactor + one more HW

--- src/Lesson8nonaccessmodifiers/L8HW/Lesson8HW.java
+++ src/Lesson8nonaccessmodifiers/L8HW/Lesson8HW.java
@@ -1,6 +1,21 @@
 package Lesson8nonaccessmodifiers.L8HW;
 
 public class Lesson8HW {
+
+    static final double pi = 3.14;
+    int r;
+    double roundSquare(int radius){
+        return (pi * radius * radius);
+    }
+
+    static double roundLength(int radius){
+        return (2 * pi * radius);
+    }
+
+    void showInfo(int radius){
+        System.out.println("Радиус окружности равен " + radius + "\nПлощадь круга равна " + roundSquare(radius) + "\nДлина окружности равна " + roundLength(radius));
+    }
+
     static int composition(int a, int b, int c){
         return a * b *c;
     }
@@ -14,7 +29,13 @@ public class Lesson8HW {
 
 class Lesson8HWTest {
     public static void main(String[] args) {
-        System.out.println(Lesson8HW.composition(3,4,5));
+        System.out.println("Произведение трех чисел равно " + Lesson8HW.composition(3,4,5));
         Lesson8HW.division(5,2);
+        Lesson8HW round = new Lesson8HW();
+        round.r = 4;
+        System.out.println("Площадь круга с радиусом " + round.r + " равна " + round.roundSquare(round.r));
+        System.out.println("Длина окружности с радиусом " + round.r + "равна " + Lesson8HW.roundLength(round.r));
+        System.out.println();
+        round.showInfo(round.r);
     }
 }
